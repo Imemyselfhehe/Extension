d9747c1901c66a900011b0e42a6f147175e4868d
ravali potineni <ravalip@ravalis-MacBook-Air.local>
2022-03-09 22:25:44 -0500

    Added A Button

--- popup.js
+++ popup.js
@@ -1,5 +1,14 @@
 let printDOM = document.getElementById("printDOM");
-
 chrome.storage.local.get("dom", ({ dom }) => {
   printDOM.innerHTML = dom;
 });
+document.getElementById("button").addEventListener("click", saveDynamicDataToFile);
+		function saveDynamicDataToFile() {
+		        var content =printDOM.innerHTML 
+            var blob = new Blob([content], { type: "text/plain;charset=utf-8" });
+            var url = URL.createObjectURL(blob);
+            chrome.downloads.download({
+  url: url
+});
+      console.log("worked");
+        }
