179997aa1e4b0c042b6246cebc8d1c42508b4526
ravali potineni <ravalip@ravalis-MacBook-Air.local>
2022-03-28 11:04:13 -0400

    Added Code to Filter The Tags

--- content.js
+++ content.js
@@ -2,6 +2,48 @@ function setDom() {
   let dom = document.body.innerHTML;
   chrome.storage.local.set({ dom });
 }
+function gotMessage(message) {
+  if (message.txt == "hello") {
+   var modal = document.createElement('div');
+modal.className = "modal";
+ var el = document.getElementsByTagName(message.value);
+ var tag = document.createElement(message.value);
+ tag.appendChild(el[0]);
+ for (const value of el) {
+  tag.appendChild(value)
+  }
 
-chrome.runtime.onMessage.addListener(setDom);
+ modal.appendChild(tag);
+  console.log(modal);
+document.body.appendChild(modal);
+var styles = `
+ .modal {
+    position: fixed;
+    top: 25%;
+    left: 10%;
+    width: 1000px;
+    line-height: 200px;
+    height: 600px;
+    margin-left: -10px;
+    margin-top: -10px;
+    background-color: #C0C0C0;
+    text-align: center;
+    z-index: 10;
+    outline: 9999px solid rgba(0,0,0,0.5);
+    max-height: calc(100vh - 210px);
+    overflow-y: auto;
+}
+`
+
+var styleSheet = document.createElement("style")
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet)
+ 
+  } else {
+    setDom();
+  }
+}
+chrome.runtime.onMessage.addListener(gotMessage);
 document.body.onload = setDom;
+
+
--- popup.js
+++ popup.js
@@ -1,14 +1,65 @@
 let printDOM = document.getElementById("printDOM");
+var temp = document.createElement("body");
 chrome.storage.local.get("dom", ({ dom }) => {
-  printDOM.innerHTML = dom;
+  temp.innerHTML = dom;
+  //document.body.appendChild(temp.getElementsByTagName("p")[0]);
+  //printDOM.innerHTML = dom;
 });
+document.getElementById("element").addEventListener("click", getOption);
+function getOption() {
+/*
+    var w = 440;
+    var h = 220;
+    const dualScreenLeft = window.screenLeft !==  undefined ? window.screenLeft : window.screenX;
+    const dualScreenTop = window.screenTop !==  undefined   ? window.screenTop  : window.screenY;
+
+    const width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+    const height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+    const systemZoom = width / window.screen.availWidth;
+    const left = (width - w) / 2 / systemZoom + dualScreenLeft
+    const top = (height - h) / 2 / systemZoom + dualScreenTop
+
+ var win = window.open("", "Title", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=${w / systemZoom}, height=${h / systemZoom}, top=${top}, left=${left}");
+ if (window.focus) win.focus();
+//win.document.body.innerHTML = "HTML";
+
+  var el = temp.getElementsByTagName(document.getElementById('element').elements[0].value);
+
+  console.log(el)
+    var tag = win.document.createElement(document.getElementById('element').elements[0].value);
+      tag.appendChild(el[0]);
+    win.document.body.appendChild(tag);
+for (const value of el) {
+  tag.appendChild(value)
+  }
+*/
+
+let params = {
+  active: true,
+  currentWindow: true
+};
+let input = document.getElementById('element').elements[0].value;
+
+chrome.tabs.query(params, gotTabs);
+
+function gotTabs(tabs) {
+  let msg = {
+    txt: "hello",
+    value: input
+  }
+  chrome.tabs.sendMessage(tabs[0].id,msg);
+
+}
+window.close();
+}
+
 document.getElementById("button").addEventListener("click", saveDynamicDataToFile);
 		function saveDynamicDataToFile() {
-		        var content =printDOM.innerHTML 
+		        var content =temp.innerHTML 
             var blob = new Blob([content], { type: "text/plain;charset=utf-8" });
             var url = URL.createObjectURL(blob);
             chrome.downloads.download({
   url: url
 });
-      console.log("worked");
-        }
+}
